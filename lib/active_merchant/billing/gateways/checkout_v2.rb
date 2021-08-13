003483d7460339d0fcf9e0c942de86e27bd10c8c
Harish Ramesh <harish@inai.io>
2021-08-13 13:11:37 +0530

    Added psp tokenization model

--- lib/active_merchant/billing/gateways/checkout_v2.rb
+++ lib/active_merchant/billing/gateways/checkout_v2.rb
@@ -109,8 +109,8 @@ module ActiveMerchant #:nodoc:
 
       def add_payment_method(post, payment_method, options)
         post[:source] = {}
-        if options[:token_type] == 'id'
-          post[:source][:type] = options[:token_type]
+        if payment_method.is_a?(PSPTokenizedCard)
+          post[:source][:type] = payment_method[:type]
           post[:source][:id] = payment_method[:token]
           return
         end
