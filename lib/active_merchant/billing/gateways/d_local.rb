a7a92bcee3d49fef9e947d8272fa6e32e38910c1
Harish Ramesh <harish@inai.io>
2021-08-19 11:36:15 +0530

    Changing attr accessor

--- lib/active_merchant/billing/gateways/d_local.rb
+++ lib/active_merchant/billing/gateways/d_local.rb
@@ -26,6 +26,9 @@ module ActiveMerchant #:nodoc:
       def initiate(money, payment, options = {})
         post = {}
         add_auth_purchase_params(post, money, payment, 'initiate', options)
+        post[:notification_url] = options[:notification_url] if options[:notification_url]
+        post[:callback_url] = options[:callback_url] if options[:callback_url]
+        p post
 
         commit('initiate', post, options)
       end
@@ -246,6 +249,7 @@ module ActiveMerchant #:nodoc:
         when 'refund'
           'refunds'
         when 'initiate'
+          'payments'
         when 'capture'
           'payments'
         when 'void'
